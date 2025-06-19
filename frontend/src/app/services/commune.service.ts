1ff851bb0d1f73d75ee2952ab4c40250d20d79ec
aschlegle <antoine.schlegel01@gmail.com>
2025-06-19 16:51:38 +0200

    factorisation communes, departements et regions

--- frontend/src/app/services/commune.service.ts
+++ frontend/src/app/services/commune.service.ts
@@ -1,38 +1,18 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ICommune } from '@app/interfaces/commune.interface';
 import { Commune } from '@app/models/commune.model';
-import { Observable, map } from 'rxjs';
+import { BaseEntityService } from './base-entity.service';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CommuneService {
+export class CommuneService extends BaseEntityService<Commune, ICommune> {
 
-    private GET_ALL_URL = environment.flask_server+'communes';
     private BASE_URL = environment.flask_server+'commune/';
-    private http = inject(HttpClient);
     
-    //Récupère toutes les communes
-    getAll(): Observable<Commune[]> {
-      return this.http.get<ICommune[]>(this.GET_ALL_URL).pipe(
-        map(communeJsonArray => {
-          return communeJsonArray.map<Commune>(
-            communeJson => Commune.fromJson(communeJson)
-          )
-        })
-      );
+    constructor() {
+      super('communes', Commune, 'nom_com');
     }
   
-    //Trie la liste par ordre alpha
-    sortByName(objArray:Commune[]){
-      objArray.sort(function(a, b) {
-        var textA = a.nom_com.toUpperCase();
-        var textB = b.nom_com.toUpperCase();
-        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-      })
-    }
-  
-    
   }
