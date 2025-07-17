7c385d8c2bb0fcc3735c38949d69e69f952bf3ff
BGE31-Thomas <tpjarles@gmail.com>
2025-07-17 13:36:44 +0200

    factorisation entités geographiques > Antoine

--- frontend/src/app/services/commune.service.ts
+++ frontend/src/app/services/commune.service.ts
@@ -1,38 +1,16 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ICommune } from '@app/interfaces/commune.interface';
 import { Commune } from '@app/models/commune.model';
-import { Observable, map } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { BaseEntityService } from './base-entity.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class CommuneService {
-
-    private GET_ALL_URL = environment.flask_server+'communes';
-    private BASE_URL = environment.flask_server+'commune/';
-    private http = inject(HttpClient);
+export class CommuneService extends BaseEntityService<Commune, ICommune> {
     
-    //Récupère toutes les communes
-    getAll(): Observable<Commune[]> {
-      return this.http.get<ICommune[]>(this.GET_ALL_URL).pipe(
-        map(communeJsonArray => {
-          return communeJsonArray.map<Commune>(
-            communeJson => Commune.fromJson(communeJson)
-          )
-        })
-      );
+    constructor() {
+      super('communes', Commune, 'nom_com');
     }
   
-    //Trie la liste par ordre alpha
-    sortByName(objArray:Commune[]){
-      objArray.sort(function(a, b) {
-        var textA = a.nom_com.toUpperCase();
-        var textB = b.nom_com.toUpperCase();
-        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-      })
-    }
-  
-    
-  }
+}
--- frontend/src/app/services/region.service.ts
+++ frontend/src/app/services/region.service.ts
@@ -1,35 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { IRegion } from '@app/interfaces/region.interface';
 import { Region } from '@app/models/region.model';
-import { Observable, map } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { BaseEntityService } from './base-entity.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RegionService {
+export class RegionService extends BaseEntityService<Region, IRegion> {
 
-  private GET_ALL_URL = environment.flask_server+'regions';
-  private http = inject(HttpClient);
-  
-
-  getAll(): Observable<Region[]> {
-    return this.http.get<IRegion[]>(this.GET_ALL_URL).pipe(
-      map(regionJsonArray => {
-        return regionJsonArray.map<Region>(
-          regionJson => Region.fromJson(regionJson)
-        )
-      })
-    );
-  }
-
-  sortByName(objArray:Region[]){
-    objArray.sort(function(a, b) {
-      var textA = a.nom_reg.toUpperCase();
-      var textB = b.nom_reg.toUpperCase();
-      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-    })
+  constructor() {
+    super('regions', Region, 'nom_reg');
   }
 
 }
